ebc638dd606fcb9572ddc6ce38d55738959ed99a
JannLeo <1144097453@qq.com>
2024-01-24 22:19:25 +0000

    day23

--- leetcode/day22/22.括号生成/22.括号生成/源.cpp
+++ leetcode/day22/22.括号生成/22.括号生成/源.cpp
@@ -1,12 +1,34 @@
 #include <iostream>
 #include <vector>
+#include <stack>
+#include <unordered_map>
 using namespace std;
-
-vector<string> generateParenthesis(int n) {
-
+void DFS(vector<string>& result,int deepth,int& max,string& temp_result,int right,int left) {
+	if (deepth == 2 * max) {
+		result.push_back(temp_result);
+		return;
+	}
+	if (left < max) {
+		temp_result += "(";
+		DFS(result, deepth+1, max, temp_result, right, left + 1);
+		temp_result.pop_back();
+	}
+	if (right < left) {
+		temp_result += ")";
+		DFS(result, deepth + 1, max, temp_result, right+1, left);
+		temp_result.pop_back();
+	}
 }
-
+//不能在回溯的时候带返回type类型
+vector<string> generateParenthesis(int n) {
+	vector<string> result;
+	string origin = "(";
+	DFS(result, 1, n,origin,0,1);
+	return result;
+}
+//["((()))","(()())","(())()","()(())","()()()"]
 void main() {
 	int n = 3;
-	generateParenthesis(n);
+	vector<string> a = generateParenthesis(n);
+
 }
