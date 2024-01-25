9fc4d4e8a37fcef5c2348d5c87990c9452378783
JannLeo <1144097453@qq.com>
2024-01-25 19:10:34 +0000

    day23

---
+++ leetcode/day23/23.合并k个升序链表/23.合并k个升序链表/源.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+struct ListNode {
+	int val;
+	ListNode* next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+ListNode* combinetoone(ListNode* p1,ListNode* p2) {
+	ListNode* head=new ListNode();
+	if (!p1 && !p2)
+		return NULL;
+	if (!p1) {
+		return head = p2;
+	}
+	else if (!p2) {
+		return head = p1;
+	}
+	if (p1->val > p2->val) {
+		head->val = p2->val;
+		p2 = p2->next;
+	}
+	else {
+		head->val = p1->val;
+		p1 = p1->next;
+	}
+	ListNode* p3 = head;
+	while (p1 && p2) {
+		if (p1->val > p2->val) {
+			p3->next = new ListNode(p2->val);
+			p2 = p2->next;
+		}
+		else {
+			p3->next = new ListNode(p1->val);
+			p1 = p1->next;
+		}
+		p3 = p3->next;
+	}
+	if (!p1) {
+		p3->next=p2;
+	}
+	else if (!p2) {
+		p3->next = p1;
+	}
+	return head;
+}
+ListNode* dividelists(vector<ListNode*> lists,int left,int right) {	
+	if (left == right)
+		return lists[left];
+	if (left > right)
+		return NULL;
+	int mid = (left + right) / 2;
+	ListNode* p1=dividelists(lists, left, mid);
+	ListNode* p2= dividelists(lists, mid+1, right);
+	return combinetoone(p1, p2);
+}
+
+ListNode* mergeKLists(vector<ListNode*>& lists) {
+	if (lists.size() == 0) {
+		return NULL;
+	}
+	else if (lists.size() == 1) {
+		return lists[0];
+	}
+
+	//分治法
+	return dividelists(lists, 0, lists.size() - 1);
+}
+void main() {
+	//初始化
+	ListNode* p = new ListNode();
+	p->val = 1;
+	p->next = new ListNode(4);
+	p->next->next = new ListNode(5);
+	ListNode* p2 = new ListNode();
+	p2->val = 1;
+	p2->next = new ListNode(3);
+	p2->next->next = new ListNode(4);
+	ListNode* p3 = new ListNode();
+	p3->val = 2;
+	p3->next = new ListNode(6);
+	vector<ListNode*> lists;
+	lists.push_back(p);
+	lists.push_back(p2);
+	lists.push_back(p3);
+	ListNode* result = mergeKLists(lists);
+
+}
