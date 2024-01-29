748bb6d323a3a99756bf073711b2ee5d6ff0d31b
JannLeo <1144097453@qq.com>
2024-01-29 16:23:41 +0000

    autopush

--- leetcode/day25/25.K个一组翻转链表/25.K个一组翻转链表/源.cpp
+++ leetcode/day25/25.K个一组翻转链表/25.K个一组翻转链表/源.cpp
@@ -10,82 +10,137 @@ struct ListNode {
 	ListNode(int x, ListNode* next) : val(x), next(next) {}
 	
 };
-void reverselist(ListNode* head,ListNode* hlast,ListNode* lastnode, int k) {
-	ListNode* temp = lastnode;
-	while (head != lastnode) {
-		ListNode* hnext = head->next;
-		hlast->next = hnext;
-		head->next = temp->next;
-		temp->next = head;
-		// 2 3 1 4 5
-		// hnext->...->temp->head->...
-		head = hnext;
+//void reverselist(ListNode* head,ListNode* hlast,ListNode* lastnode, int k) {
+//	ListNode* temp = lastnode;
+//	while (head != lastnode) {
+//		ListNode* hnext = head->next;
+//		hlast->next = hnext;
+//		head->next = temp->next;
+//		temp->next = head;
+//		// 2 3 1 4 5
+//		// hnext->...->temp->head->...
+//		head = hnext;
+//	}
+//	return;
+//}
+//ListNode* reverseKGroup(ListNode* head, int k) {
+//	if (!head)
+//		return NULL;
+//	ListNode* p = head;
+//	ListNode* result=head;
+//	bool flag = 1;
+//	int times = 0;
+//	int temp = k - 1;
+//	while (temp--) {
+//		if (p->next) {
+//			p = p->next;
+//		}
+//		else {
+//			flag = 0;
+//		}
+//	}
+//	if (flag) {
+//		result = p;
+//		while (head != p) {
+//			// 2
+//			ListNode* hnext = head->next;
+//			head->next = p->next;
+//			p->next = head;
+//			head = hnext;
+//		}
+//	}
+//	// 3 2 1 4 5 6 7 8 9 10
+//	temp = k - 1;
+//	while (temp--) {
+//		if (p->next&&p->next->next) {
+//			p = p->next->next;
+//			head = head->next;
+//		}
+//		else {
+//			flag = 0;
+//		}
+//	}
+//	if (p->next)
+//		p = p->next;
+//	else
+//		flag = 0;
+//	// 3 2 1 4 5 6 7 8 9
+//	while (flag) {
+//		reverselist(head->next, head, p, k);
+//		// 3 2 1 6 5 4 7 8 9
+//		temp = k-1;
+//		while (temp--) {
+//			if (p->next && p->next->next) {
+//				p = p->next->next;
+//				head = head->next;
+//			}
+//			else {
+//				flag = 0;
+//			}
+//		}
+//		head = head->next;
+//		if (p->next)
+//			p = p->next;
+//		else
+//			flag = 0;
+//	}
+//	return result;
+//}
+
+//优化之后的解法
+//解决两个问题：
+//1、如何取消特殊判定 就是第一次翻转特殊考虑如何精简掉
+//2、如何便携地找出需要返回的链表而不用额外占用空间去寻找
+//3、如何再子函数返回修改后的位置而不需要重新定位
+pair<ListNode*,ListNode*> reverseKnode(ListNode* prehead,ListNode* endnode,int k) {
+	ListNode* p = prehead->next;
+	//0 3 2 1 4 5 6
+	// 
+	//0 1 2 3 4 5 6
+	//0 2 3 1 4 5 6
+	//0 3 2 1 4 5 6 7 8 9
+	ListNode* head = prehead->next;
+	while (head != endnode) {
+		prehead->next = head->next;
+		head->next = endnode->next;
+		endnode->next = head;
+		head = prehead->next;
 	}
-	return;
+	ListNode* p2 = p;
+	while (k--) {
+		if (p2->next) {
+			p2 = p2->next;
+		}
+		else {
+			return { p,NULL };
+		}
+	}
+	return { p,p2 };
 }
 ListNode* reverseKGroup(ListNode* head, int k) {
-	if (!head)
-		return NULL;
-	ListNode* p = head;
-	ListNode* result=head;
-	bool flag = 1;
-	int times = 0;
-	int temp = k - 1;
-	while (temp--) {
-		if (p->next) {
-			p = p->next;
-		}
-		else {
-			flag = 0;
-		}
-	}
-	if (flag) {
-		result = p;
-		while (head != p) {
-			// 2
-			ListNode* hnext = head->next;
-			head->next = p->next;
-			p->next = head;
-			head = hnext;
-		}
-	}
-	// 3 2 1 4 5 6 7 8 9 10
-	temp = k - 1;
-	while (temp--) {
-		if (p->next&&p->next->next) {
-			p = p->next->next;
-			head = head->next;
-		}
-		else {
-			flag = 0;
-		}
-	}
-	if (p->next)
-		p = p->next;
-	else
-		flag = 0;
-	// 3 2 1 4 5 6 7 8 9
-	while (flag) {
-		reverselist(head->next, head, p, k);
-		// 3 2 1 6 5 4 7 8 9
-		temp = k-1;
-		while (temp--) {
-			if (p->next && p->next->next) {
-				p = p->next->next;
-				head = head->next;
-			}
-			else {
-				flag = 0;
-			}
-		}
-		head = head->next;
-		if (p->next)
-			p = p->next;
+	if (k == 1 || !head)
+		return head;
+	ListNode* prehead = new ListNode();
+	ListNode* result = prehead;
+	prehead->next = head;
+	int num = k;
+	// 1 2 3 4 5 6
+	ListNode* endnode = prehead;
+	while (num--) {
+		if (endnode->next)
+			endnode = endnode->next;
 		else
-			flag = 0;
+			return NULL;
 	}
-	return result;
+	pair<ListNode*, ListNode*> temp;
+	while (endnode) {
+		temp=reverseKnode(prehead,endnode, k);
+		prehead = temp.first;
+		endnode = temp.second;
+	}
+	return result->next;
 }
+
 
 void main() {
 	ListNode head, * tail = &head;
@@ -96,7 +151,7 @@ void main() {
 	tail = tail->next;
 	tail->next = new ListNode(3);
 	tail = tail->next;
-	/*tail->next = new ListNode(4);
+	tail->next = new ListNode(4);
 	tail = tail->next;
 	tail->next = new ListNode(5);
 	tail = tail->next;
@@ -109,6 +164,6 @@ void main() {
 	tail->next = new ListNode(9);
 	tail = tail->next;
 	tail->next = new ListNode(10);
-	tail = tail->next;*/
+	tail = tail->next;
 	reverseKGroup(head.next, k);
 }
---
+++ leetcode/day27/27.移除元素/27.移除元素/源.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int removeElement(vector<int>& nums, int val) {
+	if (nums.size() ==0) {
+		return 0;
+	}
+	int i = 0;
+	int length = nums.size();
+	int gap = 0;
+	while ( i < length) {
+		if (val == nums[i]) {
+			gap++;
+		}
+		else {
+			nums[i - gap] = nums[i];
+			
+		}
+		i++;
+	}
+	return i-gap;
+}
+void main() {
+	vector<int> nums({1});
+	int val = 1;
+	removeElement(nums, val);
+}
