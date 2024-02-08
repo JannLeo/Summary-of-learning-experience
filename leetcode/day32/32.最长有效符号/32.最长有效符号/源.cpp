43c2063e3241af871d39b6b135c2b798193bc4bd
刘俊楠 <34327091+JannLeo@users.noreply.github.com>
2024-02-08 09:10:20 +0000

    autopush

--- leetcode/day32/32.最长有效符号/32.最长有效符号/源.cpp
+++ leetcode/day32/32.最长有效符号/32.最长有效符号/源.cpp
@@ -2,22 +2,47 @@
 #include <vector>
 #include <stack>
 using namespace std;
+//int longestValidParentheses(string s) {
+//	stack<int> a;
+//	int max_num = 0;
+//	a.push(-1);
+//	for (int i = 0; i < s.size(); i++) {
+//		if (s[i] == '(') {
+//			a.push(i);
+//		}
+//		else {
+//			a.pop();
+//			if (!a.empty()) {
+//				max_num = max(i - a.top(), max_num);
+//			}
+//			else {
+//				a.push(i);
+//			}
+//		}
+//	}
+//	return max_num;
+//}
 int longestValidParentheses(string s) {
-	stack<int> a;
+	int length = s.size();
+	vector<int> temp(length, 0);
 	int max_num = 0;
-	a.push(-1);
-	for (int i = 0; i < s.size(); i++) {
-		if (s[i] == '(') {
-			a.push(i);
-		}
-		else {
-			a.pop();
-			if (!a.empty()) {
-				max_num = max(i - a.top(), max_num);
+	for (int i = 1; i < length; i++) {
+		if (s[i] == ')') {
+			if (s[i - 1] == '(') {
+				// ()()
+				temp[i] = i - 2 > 0 ? temp[i - 2] + 2 : 2;
 			}
 			else {
-				a.push(i);
+				if (i - temp[i - 1] - 1 >= 0) {
+					// 0 1 2 3 4 5 6 7
+					// ) ( ) ( ( ) ) )
+					// 0 0 2 0 0 2 6 0
+					if (s[i - temp[i - 1] - 1] == '(') {
+						temp[i] = i - 2 - temp[i - 1] > 0 ? temp[i - 1] + 2 + temp[i - 2 - temp[i - 1]] : temp[i - 1] + 2;
+					}
+				}
 			}
+			max_num = max(temp[i], max_num);
 		}
 	}
 	return max_num;
