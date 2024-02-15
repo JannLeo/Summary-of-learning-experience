7de68d326952c981e7bfd07441dd2748ebca215b
刘俊楠 <34327091+JannLeo@users.noreply.github.com>
2024-02-15 13:59:05 +0000

    autopush

--- leetcode/day36/36.有效的数独/36.有效的数独/源.cpp
+++ leetcode/day36/36.有效的数独/36.有效的数独/源.cpp
@@ -3,45 +3,67 @@
 #include <unordered_map>
 using namespace std;
 bool isValidSudoku(vector<vector<char>>& board) {
-
-}
-bool isValidSudoku(vector<vector<char>>& board) {
-    vector<unordered_map<char, int>> a(27);
+    vector<int> temp(27, 0);
     for (int i = 0; i < 9; i++) {
         for (int j = 0; j < 9; j++) {
-            //按行区别
-            if (board[i][j] != '.') {
-                if (!a[i][board[i][j]] && !a[(i / 3) * 3 + j / 3 + 18][board[i][j]]) {
-                    a[i][board[i][j]]++;
-                    a[(i / 3) * 3 + j / 3 +18][board[i][j]]++;
+            if (board[i][j] !='.') {
+                int a = 1<<(board[i][j] - '0');
+                if (!(a & temp[i]) && !(a & temp[(i / 3) * 3 + j / 3 + 18])) {
+                    temp[i] += a;
+                    temp[(i / 3) * 3 + j / 3 + 18] += a;
                 }
-                else 
+                else
                     return false;
             }
             if (board[j][i] != '.') {
-                if (a[i + 9][board[j][i]] == 0) {
-                    a[i + 9][board[j][i]]++;
+                int a = 1 << (board[j][i] - '0');
+                if (!(a & temp[i + 9])) {
+                    temp[i + 9] += a;
                 }
-                else return false;
+                else
+                    return false;
             }
         }
-    } 
+    }
     return true;
 }
+//bool isValidSudoku(vector<vector<char>>& board) {
+//    vector<unordered_map<char, int>> a(27);
+//    for (int i = 0; i < 9; i++) {
+//        for (int j = 0; j < 9; j++) {
+//            //按行区别
+//            if (board[i][j] != '.') {
+//                if (!a[i][board[i][j]] && !a[(i / 3) * 3 + j / 3 + 18][board[i][j]]) {
+//                    a[i][board[i][j]]++;
+//                    a[(i / 3) * 3 + j / 3 +18][board[i][j]]++;
+//                }
+//                else 
+//                    return false;
+//            }
+//            if (board[j][i] != '.') {
+//                if (a[i + 9][board[j][i]] == 0) {
+//                    a[i + 9][board[j][i]]++;
+//                }
+//                else return false;
+//            }
+//        }
+//    } 
+//    return true;
+//}
 void main() {
     // 1,3
     // 2,5
     // j/3+ (i/3)*3
     vector<vector<char>> board = {
-    {'.','.','.','.','5','.','.','1','.'},
-    {'.','4','.','3','.','.','.','.','.'},
-    {'.','.','.','.','.','3','.','.','1'},
-    {'8','.','.','.','.','.','.','2','.'},
-    {'.','.','2','.','7','.','.','.','.'},
-    {'.','1','5','.','.','.','.','.','.'},
-    {'.','.','.','.','.','2','.','.','.'},
-    {'.','2','.','9','.','.','.','.','.'},
-    {'.','.','4','.','.','.','.','.','.'}
+    {'5', '3', '.', '3', '7', '.', '.', '.', '.'},
+    {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+    {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+    {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+    {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+    {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+    {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+    {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+    {'.', '.', '.', '.', '8', '.', '.', '8', '9'}
     };
     bool result =  isValidSudoku(board);
 }
