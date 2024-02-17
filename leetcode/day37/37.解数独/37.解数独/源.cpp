168b7d5c18f222657eb35c201dbcd526601f17ab
刘俊楠 <34327091+JannLeo@users.noreply.github.com>
2024-02-17 14:49:42 +0000

    autopush

--- leetcode/day37/37.解数独/37.解数独/源.cpp
+++ leetcode/day37/37.解数独/37.解数独/源.cpp
@@ -2,48 +2,34 @@
 #include <vector>
 #include<unordered_map>
 using namespace std;
-bool judgeSuDo(vector<int> temp,int i,int j,int num) {
-    int a = 1 << num;
-    if (!(temp[i] & a) && !(temp[i + 9] & a) && !(temp[i / 3 * 3 + j / 3 + 18] & a)) {
-        temp[i] += a;
-        temp[i + 9] += a;
-        temp[i / 3 * 3 + j / 3 + 18] += a;
-        return true;
+bool judgeSuDo(vector<vector<char>>& board,int x,int y,int num) {
+    for (int i = 0; i < 9; i++) {
+        if (board[x][i] == num+'0') return false;
+        if (board[i][y] == num+'0') return false;
+        // [0,4]  0 3
+        if (board[(x / 3) * 3 + i / 3][(y / 3) * 3 + i % 3] == num + '0') return false;
     }
-    return false;
+    return true;
 }
-void DFS(vector<vector<char>>& board,int pos) {
-    if (pos / 9 == 9)
-        return;
-    int xPos = pos / 9;
-    int yPos = pos % 9;
-
-}
-void solveSudoku(vector<vector<char>>& board) {
-    vector<int> temp(27, 0);
-    vector<int> dot;
+bool DFS(vector<vector<char>>& board) {
     for (int i = 0; i < 9; i++) {
         for (int j = 0; j < 9; j++) {
-            if (board[i][j] != '.') {
-                int a = 1 << (board[i][j] - '0');
-                if (!(temp[i] & a) && (!(temp[i / 3 * 3 + j / 3 + 18] & a))) {
-                    temp[i] += a;
-                    temp[i / 3 * 3 + j / 3 + 18] += a;
+            if (board[i][j] != '.') continue;
+            for (int k = 1; k <= 9; k++) {
+                if (judgeSuDo(board, i, j, k)) {
+                    board[i][j] = k + '0';
+                    if (DFS(board)) return true;
                 }
             }
-            else {
-                dot.emplace_back(i * 9 + j + 1);
-            }
-            if (board[j][i] != '.') {
-                int a = 1 << (board[j][i] - '0');
-                if (!(temp[i + 9] & a)) {
-                    temp[i + 9] += a;
-                }
-            }
+            board[i][j] = '.';
+            return false;
         }
     }
-    DFS(board, 0);
+    return true;
+}
+void solveSudoku(vector<vector<char>>& board) {
 
+    DFS(board);
 }
 void main() {
 	vector<vector<char>> board = {
@@ -58,4 +44,5 @@ void main() {
         {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
     };
 	solveSudoku(board);
+    cout << "end" << endl;
 }
