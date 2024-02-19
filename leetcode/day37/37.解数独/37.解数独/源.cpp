8c350190a14ef8926261ed8d9e1f1cc4d1f5163b
刘俊楠 <34327091+JannLeo@users.noreply.github.com>
2024-02-19 13:17:01 +0000

    autopush

--- leetcode/day37/37.解数独/37.解数独/源.cpp
+++ leetcode/day37/37.解数独/37.解数独/源.cpp
@@ -12,29 +12,35 @@ using namespace std;
 //    }
 //    return true;
 //}
-bool judgeSuDo(vector<bitset<9>>& temp,pair<int,int> point, int num) {
+//bool judgeSuDo(vector<bitset<9>>& temp,pair<int,int> point, int num) {
+//    int x = point.first;
+//    int y = point.second;
+//    if (temp[x][num - 1]) return false;
+//    if (temp[y + 9][num - 1]) return false;
+//    if (temp[x / 3 * 3 + y/3 + 18][num - 1]) return false;
+//    
+//    return true;
+//}
+bool judgeSuDo(vector<bitset<9>>& temp, pair<int, int>& point, int num) {
     int x = point.first;
     int y = point.second;
-    if (temp[x][num - 1]) return false;
-    if (temp[y + 9][num - 1]) return false;
-    if (temp[x / 3 * 3 + y/3 + 18][num - 1]) return false;
-    
-    return true;
+    return !(temp[x][num] || temp[y + 9][num] || temp[x / 3 * 3 + y / 3 + 18][num]);
 }
-bool DFS(vector<vector<char>>& board,vector<bitset<9>> temp,vector<pair<int,int>> point) {
+
+bool DFS(vector<vector<char>>& board,vector<bitset<9>>& temp,vector<pair<int,int>>& point,int pos) {
     auto length = point.size();
-    for (int i = 0; i < length; i++) {
+    for (int i = pos; i < length; i++) {
         int x = point[i].first;
         int y = point[i].second;
         int a = board[x][y] - '0';
         if (a > 0) continue;
         for (int j = 1; j <= 9; j++) {
-            if (judgeSuDo(temp, point[i], j)) {
+            if (judgeSuDo(temp, point[i], j-1)) {
                 board[x][y] = j + '0';
                 temp[x][j - 1] = 1;
                 temp[y + 9][j - 1] = 1;
                 temp[x / 3 * 3 + y/3 + 18][j - 1] = 1;
-                if (DFS(board, temp, point)) return true;
+                if (DFS(board, temp, point, pos + 1)) return true;
                 temp[x][j - 1] = 0;
                 temp[y + 9][j - 1] = 0;
                 temp[x / 3 * 3 + y/3 + 18][j - 1] = 0;
@@ -65,6 +71,15 @@ void solveSudoku(vector<vector<char>>& board) {
     for (int i = 0; i < 9; i++) {
         for (int j = 0; j < 9; j++) {
             if (board[i][j] != '.') {
+                int a = board[i][j] - '1';
+                temp[i][a] = 1;
+                temp[i / 3 * 3 + j / 3 + 18][a] = 1;
+                temp[j + 9][a] = 1;
+            }
+            else {
+                point.emplace_back(make_pair(i, j));
+            }
+            /*if (board[i][j] != '.') {
                 int a = board[i][j] - '0';
                 temp[i][a - 1] = 1;
                 temp[i / 3 * 3 + j / 3 + 18][a - 1] = 1;
@@ -75,10 +90,10 @@ void solveSudoku(vector<vector<char>>& board) {
             if (board[j][i] != '.') {
                 int a = board[j][i] - '0';
                 temp[i + 9][a - 1] = 1;
-            }
+            }*/
         }
     }
-    DFS(board,temp,point);
+    DFS(board,temp,point,0);
 }
 int main() {
 	vector<vector<char>> board = {
