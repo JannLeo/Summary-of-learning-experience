a25e692277414fe1c3a5ac3e9fe20d85ef052c51
刘俊楠 <34327091+JannLeo@users.noreply.github.com>
2024-02-20 11:59:22 +0000

    autopush

---
+++ leetcode/day39/39.组合总和/39.组合总和/源.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+void DFS(vector<vector<int>>& result, vector<int>& candidates, int target,int pos) {
+	if (pos >= candidates.size() || candidates[pos] > target)
+		return;
+	if (!(target % candidates[pos])) {
+		result.push_back({ candidates[pos] });
+	}
+	int temp = target - candidates[pos];
+	if (temp == 0)
+		return;
+	else
+		DFS(result, candidates, temp, pos + 1);
+
+}
+vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+
+	vector<vector<int>> result;
+	if (candidates[0] > target)
+		return result;
+	DFS(result, candidates, target, 0);
+}
+int main() {
+	vector<int> candidates({ 2,3,6,7 });
+	int target = 7;
+	vector<vector<int>> result;
+
+	result =  combinationSum(candidates, target);
+	return 0;
+}
---
+++ master/Modern Industry Practice/Summarize/Week17.md
@@ -0,0 +1,8 @@
+- 资源的浪费  和资源的垄断
+- 资源可持续利用的八项原则
+- TSMC（台积电）的用水与环境消耗情况
+- Restriction of Hazardous Substances Directive 有害物质限制指令
+- ![image-20240220095117186](Week17.assets/image-20240220095117186.png)
+- 
+
+- 可持续性是至关重要的
