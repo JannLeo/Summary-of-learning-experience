23a803864e3a42d89ddd6f77bb929c1eb4f3d1c9
刘俊楠 <34327091+JannLeo@users.noreply.github.com>
2024-02-20 09:15:57 +0000

    autopush

--- leetcode/day38/38.外观数列/38.外观数列/源.cpp
+++ leetcode/day38/38.外观数列/38.外观数列/源.cpp
@@ -1,20 +1,37 @@
 #include <iostream>
-#include <unordered_map>
+#include <stack>
 #include <vector>
 using namespace std;
 
-void recursion(string& temp,int times) {
-
+string recursion(string& temp,int times) {
+	if (times == 0)
+		return temp;
+	int num = 0;
+	int length = temp.size();
+	char base = temp[0];
+	string result = "";
+	for (int i = 0; i < length; i++) {
+		if (base == temp[i]) {
+			num++;
+		}
+		else{
+			result += (num + '0');
+			result += base;
+			base = temp[i];
+			num = 1;
+		}
+		if (i == length - 1) {
+			result += (num + '0');
+			result += base;
+		}
+	}
+	return recursion(result, times - 1);
 }
 string countAndSay(int n) {
 	string result = "1";
-	unordered_map<char, int> record;
-	for (int i = 0; i < 9; i++) {
-		record[i + '0'] = 0;
-	}
 	if (n == 1)
 		return result;
-	recursion(result, n - 1);
+	result = recursion(result, n - 1);
 	return result;
 }
 
