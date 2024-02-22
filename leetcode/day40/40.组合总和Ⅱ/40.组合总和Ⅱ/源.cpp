83899a42b80a22e3eea4f928c60c40e248fe64e0
刘俊楠 <34327091+JannLeo@users.noreply.github.com>
2024-02-22 12:32:39 +0000

    autopush

---
+++ leetcode/day40/40.组合总和Ⅱ/40.组合总和Ⅱ/源.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include<algorithm>
+using namespace std;
+void DFS(vector<int>& candidates, int target, int pos,vector<vector<int>> &result,vector<int> &temp) {
+	if (target == 0) {
+		result.push_back(temp);
+		return;
+	}
+	for (int i = pos; i < candidates.size() && candidates[i] <= target; i++) {
+		if (i > pos && candidates[i] == candidates[i - 1]) {
+			// 跳过当前递归层次的重复元素
+			// 1 1 2  target = 2
+			continue;
+		}
+		temp.push_back(candidates[i]);
+		DFS(candidates, target - candidates[i], i + 1, result, temp);
+		temp.pop_back();
+	}
+}
+vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+	sort(candidates.begin(), candidates.end());
+	vector<vector<int>> result;
+	if (target < candidates[0])
+		return result;
+	vector<int> temp;
+	DFS(candidates, target, 0, result, temp);
+	sort(result.begin(), result.end());
+	result.erase(unique(result.begin(), result.end()), result.end());
+	return result;
+}
+int main() {
+	//[10,1,2,7,6,1,5]   1 1 2 5 6 7 10  target=8
+	//[2,5,2,1,2] 1 2 2 2 5
+	vector<int> candidates({ 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 });
+	int target = 8;
+	combinationSum2(candidates, target);
+	return 0;
+}
