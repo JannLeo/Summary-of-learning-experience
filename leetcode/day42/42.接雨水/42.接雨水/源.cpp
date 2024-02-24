7507b46154c89c8ce90822f448194d7697d4891e
刘俊楠 <34327091+JannLeo@users.noreply.github.com>
2024-02-24 12:35:27 +0000

    autopush

---
+++ leetcode/day42/42.接雨水/42.接雨水/源.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int trap(vector<int>& height) {
+
+}
+int main() {
+	vector<int> height({ 4,2,0,3,2,5 });
+	int result =  trap(height);
+	return 0;
+}
