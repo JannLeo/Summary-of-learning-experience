08fb10085acaee2985854162f2c3334537514385
刘俊楠 <34327091+JannLeo@users.noreply.github.com>
2024-03-04 15:16:56 +0000

    autopush

---
+++ leetcode/day46/46.全排列/46.全排列/源.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+void DFS(vector<int>& nums,vector<vector<int>>& result,int first,
+	vector<int>& temp,vector<bool>& flag) {
+	int n = nums.size();
+	if (first >= n) {
+		result.push_back(temp);
+		return;
+	}
+	for (int i = 0; i < n; i++) {
+		if (!flag[i]) {
+			temp.push_back(nums[i]);
+			flag[i] = true;
+			DFS(nums, result, first + 1, temp,flag);
+			temp.pop_back();
+			flag[i] = false;
+
+		}
+	}
+}
+vector<vector<int>> permute(vector<int>& nums) {
+	vector<vector<int>> result;
+	vector<bool> flag(nums.size(),0);
+	vector<int> temp;
+	DFS(nums, result, 0, temp, flag);
+	return result;
+}
+int main() {
+	vector<int> nums({ 1,2,3,4 });
+	vector<vector<int>> result = permute(nums);
+	return 0;
+}
